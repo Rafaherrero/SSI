1e43737ea1051dec2df744250c5932f1c23be0e0
Rafaherrero <rafael.herrero.13@ull.edu.es>
2016-03-01 11:18:54 +0000

    Añadidos métodos principales

--- P3/rc4.cpp
+++ P3/rc4.cpp
@@ -1,1 +1,23 @@
 #include "rc4.hpp"
+
+using namespace std;
+
+rc4::rc4 (void):
+S_(0),
+K_(0)
+{}
+
+rc4::rc4 (string semilla):
+S_(256, 0),
+K_(256, 0)
+{
+	for (int i=0; i<=S_.length();i++){
+		S[i] = i;
+		K[i] = semilla[i%semilla.length()];
+	}
+	j=0;
+	for (int i=0; i<S_.length();i++){
+		j = (j+S_[i]+K_[i])%S_.length();
+		swap(S_[i],S_[j]);
+	}
+}
--- P3/rc4.hpp
+++ P3/rc4.hpp
@@ -1,8 +1,13 @@
+#include <vector>
+
 using namespace std;
 
 class rc4{
 private:
-
+	std::vector<unsigned char> S_;
+	std::vector<unsigned char> K_;
 public:
-
+	rc4(void);
+	rc4(string semilla);
+	~rc4 (void);
 };
