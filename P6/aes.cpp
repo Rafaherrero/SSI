e4597c404e92e1a5237f971335bd4b2efc91e9da
Rafaherrero <rafael.herrero.13@ull.edu.es>
2016-04-05 11:14:42 +0100

    Añadida la modificación de la práctica 6, CBC y Cipher Stealing

---
+++ P6/aes.cpp
@@ -0,0 +1,256 @@
+#include "aes.hpp"
+
+using namespace std;
+
+aes::aes (void)
+{}
+
+//Constructor que recibe la clave y el texto a cifrar, y genera en su clase una copia del texto y del texto cifrado
+//Ademas, inicializa un vector tridimensional que almacenaran las 11 subclaves, las cuales estan formadas por vectores bidimensionales de 4x4
+//Se inicializa el contador de iteraciones a 0 y se llama al metodo encriptar
+aes::aes(std::vector<vector<unsigned char>> cl, std::vector<vector<unsigned char>> tx, unsigned int np):
+clave_(cl),
+texto_(tx),
+texto_cf_(tx),
+clave_exp_ (std::vector<vector<vector<unsigned char>>>(11, vector<vector<unsigned char>>(4, vector<unsigned char>(4)))),
+iter_(0),
+no_imprimir_(np)
+{
+	encriptar();
+}
+
+aes::~aes (void){
+}
+
+//Método que expande la clave
+void aes::expan_clave(void){
+
+	//Variables auxiliares
+	unsigned char col_ant[4],copia;
+
+	//La primera matrix de 4x4 de la clave expandida es la misma que la de la clave cifrante, por lo que la copiamos tal cual
+	for (int i=0; i<4; i++){
+		for (int j=0; j<4; j++)
+			clave_exp_[0][j][i] = clave_[j][i];
+	}
+
+	//Para las siguiente diez iteraciones realizamos el siguiente bucle 
+	for (int i=1; i<=10; i++){
+
+		//En el caso de la primera columna de cada columna, copiamos la columna anterior en una variable de copia
+		for (int j=0; j<4; j++)
+			col_ant[j]=clave_exp_[i-1][j][3];
+
+		//La variable que contiene que contiene la columna anterior la desplazamos a un byte superior de forma ciclica, por lo que el primero pasa a ser el ultimo
+		copia = col_ant[0];
+		col_ant[0] = col_ant[1];
+		col_ant[1] = col_ant[2];
+		col_ant[2] = col_ant[3];
+		col_ant[3] = copia;
+
+		//Una vez rotados, sustituimos por el byte de su posición correspondiente en la Caja S
+		col_ant[0] = cajaS_[col_ant[0]];
+		col_ant[1] = cajaS_[col_ant[1]];
+		col_ant[2] = cajaS_[col_ant[2]];
+		col_ant[3] = cajaS_[col_ant[3]];
+
+		//Al primer byte el aplicamos XOR con la posición correspondiente en la Caja RCon
+		col_ant[0] = col_ant[0] ^ cajaRcon_[i];
+
+		//Cuando hayamos realizado todo lo anterior, tenemos que sustituir los bytes de la clave expandida con los de la matriz anterior de la clave expandida
+		//y con los bytes calculados realizando un XOR
+		clave_exp_[i][0][0] = clave_exp_[i-1][0][0] ^ col_ant[0];
+		clave_exp_[i][1][0] = clave_exp_[i-1][1][0] ^ col_ant[1];
+		clave_exp_[i][2][0] = clave_exp_[i-1][2][0] ^ col_ant[2];
+		clave_exp_[i][3][0] = clave_exp_[i-1][3][0] ^ col_ant[3];
+
+		//Lo realizado anteriormente solo afecta a la primera columna de la matriz, mientras que lo que viene a continuacion afecta al resto de columnas
+		//Recorremos las columnas de la 2 a la 4, ambas inclusive
+		for (int j=1; j<4; j++){
+			//Recorremos cada fila copiando en una variable auxiliar la columna de la matriz anterior
+			for (int k=0; k<4; k++){
+				col_ant[k]=clave_exp_[i-1][k][j];
+			}
+			//Recorremos cada fila de la matriz de la clave expandida, almacenando en ella el resultado de aplicar un XOR entre la columna anterior y la de la matriz anterior
+			for (int k=0; k<4; k++){
+				clave_exp_[i][k][j] = clave_exp_[i][k][j-1] ^ col_ant[k];
+			}
+		}
+	}
+}
+
+//Metodo que aplica una XOR al texto cifrado con la clave expandida
+void aes::addRoundKey(void){
+	for (int i=0; i<4; i++){
+		for (int j=0; j<4; j++){
+			texto_cf_[j][i] = texto_cf_[j][i] ^ clave_exp_[iter_][j][i];
+		}
+	}
+}
+
+//Metodo que sustituye un byte del texto cifrado por su correspondiente en la Caja S
+void aes::subBytes(void){
+	for (int i=0; i<4; i++){
+		for (int j=0; j<4; j++){
+			texto_cf_[j][i] = cajaS_[texto_cf_[j][i]];
+		}
+	}
+}
+
+//Metodo que mueve bytes a la izquierda las matrices del texto cifrado
+void aes::shiftRow(void){
+	unsigned char copia;
+
+	copia = texto_cf_[1][0];
+	texto_cf_[1][0] = texto_cf_[1][1];
+	texto_cf_[1][1] = texto_cf_[1][2];
+	texto_cf_[1][2] = texto_cf_[1][3];
+	texto_cf_[1][3] = copia;
+
+	copia = texto_cf_[2][0];
+	texto_cf_[2][0] = texto_cf_[2][2];
+	texto_cf_[2][2] = copia;
+	copia = texto_cf_[2][1];
+	texto_cf_[2][1] = texto_cf_[2][3];
+	texto_cf_[2][3] = copia;
+
+	copia = texto_cf_[3][0];
+	texto_cf_[3][0] = texto_cf_[3][3];
+	texto_cf_[3][3] = texto_cf_[3][2];
+	texto_cf_[3][2] = texto_cf_[3][1];
+	texto_cf_[3][1] = copia;
+}
+
+//Metodo que aplica una transformacion lineal al texto cifrado
+void aes::mixColumn(void){
+
+	//Variables auxiliares
+	unsigned char a[4];
+	unsigned char b[4];
+	unsigned char h;
+
+	//Doble bucle para recorrer una matriz completa de una sola llamada al metodo
+	for (int i=0; i<4; i++){
+		for (int j=0; j<4; j++){
+			
+			//Copiamos en a el texto cifrado
+			a[j] = texto_cf_[j][i];
+
+			//Multiplicacion entre el byte del texto cifrado y 128 para comprobar si el byte del texto es mayor que 128
+			h = texto_cf_[j][i] & 0x80; 
+
+			//Multiplicamos por 2 (desplazamos un byte) el byte del texto cifrado y lo dejamos en la variable auxiliar b
+			b[j] = texto_cf_[j][i] << 1;
+
+			//En caso de que el byte sea mayor de 127 aplicamos una XOR con 27
+			if(h == 0x80)
+				b[j] = b[j] ^0x1b;
+		}
+
+		//Realizamos todas las XOR correspondientes para cada uno de los elementos de nuestra columna, ya que cada fila tiene un valor distinto
+		texto_cf_[0][i] = b[0] ^ a[3] ^ a[2] ^ b[1] ^ a[1];
+		texto_cf_[1][i] = b[1] ^ a[0] ^ a[3] ^ b[2] ^ a[2];
+		texto_cf_[2][i] = b[2] ^ a[1] ^ a[0] ^ b[3] ^ a[3];
+		texto_cf_[3][i] = b[3] ^ a[2] ^ a[1] ^ b[0] ^ a[0];
+	}
+}
+
+//Metodo que se encarga de llamar a los metodos para realizar el encriptado completo
+void aes::encriptar(void){
+
+	//En la iteracion 0 solo expandimos la clave y aplicamos una XOR al texto cifrado con la clave expandida
+	expan_clave();
+	addRoundKey();
+
+	//Informacion para imprimir por pantalla
+	if (!no_imprimir_){
+		cout << endl << endl << "=================" << " Iteracción 0 " << "=================" << endl;
+		cout << "Subclave 0:     ";
+		for (int i=0; i<4; i++){
+			for (int j=0; j<4; j++)
+				cout << hex << setfill('0') << setw(2) << int(clave_exp_ [0][j][i]);
+		}
+		cout << endl << "Texto cifrado:  ";
+		for (int i=0; i<4; i++){
+			for (int j=0; j<4; j++)
+				cout << hex << setfill('0') << setw(2) << int(texto_cf_[j][i]);
+		}
+	}
+
+	//Llamamos a cada metodo por separado 9 veces. Se aumenta el contador de iteraciones, necesario para moverse por la clave expandida
+	for (int i=1;i<10;i++){
+		iter_++;
+		subBytes();
+		shiftRow();
+		mixColumn();
+		addRoundKey();
+
+		//Informacion para imprimir por pantalla
+		if (!no_imprimir_){
+			cout << endl << endl << "=================" << " Iteracción " << iter_ << " =================" << endl;
+			cout << "Subclave " << iter_ << ":     ";
+			for (int i=0; i<4; i++){
+				for (int j=0; j<4; j++)
+					cout << hex << setfill('0') << setw(2) << int(clave_exp_ [iter_][j][i]);
+			}
+			cout << endl << "Texto cifrado:  ";
+			for (int i=0; i<4; i++){
+				for (int j=0; j<4; j++)
+					cout << hex << setfill('0') << setw(2) << int(texto_cf_[j][i]);
+			}
+		}
+	}
+
+	//En la ultima iteracion aplicamos los mismos metodos que antes, salvo que ahora no realizamos 
+	iter_++;
+	subBytes();
+	shiftRow();
+	addRoundKey();
+
+	//Informacion para imprimir por pantalla
+	if (!no_imprimir_){
+		cout << endl << endl << "=================" << " Iteracción 10 " << "=================" << endl;
+		cout << "Subclave 10:     ";
+		for (int i=0; i<4; i++){
+			for (int j=0; j<4; j++)
+				cout << hex << setfill('0') << setw(2) << int(clave_exp_ [iter_][j][i]);
+		}
+		cout << endl << "Texto cifrado:   ";
+		for (int i=0; i<4; i++){
+			for (int j=0; j<4; j++)
+				cout << hex << setfill('0') << setw(2) << int(texto_cf_[j][i]);
+		}
+		cout << endl;
+	}
+}
+
+aes::aes(std::vector<vector<unsigned char>> cl, std::vector<vector<unsigned char>> tx, std::vector<vector<unsigned char>> cl_ant):
+clave_(cl),
+texto_(tx),
+texto_cf_(tx),
+clave_exp_ (std::vector<vector<vector<unsigned char>>>(11, vector<vector<unsigned char>>(4, vector<unsigned char>(4)))),
+clave_anterior_(cl_ant),
+iter_(0),
+no_imprimir_(1)
+{
+	addRoundKey_mod();
+	encriptar();
+	clave_anterior_ = texto_cf_;
+}
+
+void aes::addRoundKey_mod(void){
+	for (int i=0; i<4; i++){
+		for (int j=0; j<4; j++){
+			texto_cf_[j][i] = texto_cf_[j][i] ^ clave_anterior_[j][i];
+		}
+	}
+}
+
+std::vector<vector<unsigned char>> aes::get_clave_anterior(void){
+	return clave_anterior_;
+}
+
+std::vector<vector<unsigned char>> aes::get_texto_encriptado(void){
+	return texto_cf_;
+}
+
---
+++ P6/main.cpp
@@ -0,0 +1,317 @@
+#include "aes.hpp"
+
+using namespace std;
+
+int main (void){
+	std::vector<vector<unsigned char>> clave(4, vector<unsigned char>(4));
+
+	clave[0][0] = 0x00;
+	clave[1][0] = 0x01;
+	clave[2][0] = 0x02;
+	clave[3][0] = 0x03;
+	clave[0][1] = 0x04;
+	clave[1][1] = 0x05;
+	clave[2][1] = 0x06;
+	clave[3][1] = 0x07;
+	clave[0][2] = 0x08;
+	clave[1][2] = 0x09;
+	clave[2][2] = 0x0A;
+	clave[3][2] = 0x0B;
+	clave[0][3] = 0x0C;
+	clave[1][3] = 0x0D;
+	clave[2][3] = 0x0E;
+	clave[3][3] = 0x0F;
+
+	std::vector<vector<unsigned char>> texto(4, vector<unsigned char>(4));
+
+	texto[0][0] = 0x00;
+	texto[1][0] = 0x11;
+	texto[2][0] = 0x22;
+	texto[3][0] = 0x33;
+	texto[0][1] = 0x44;
+	texto[1][1] = 0x55;
+	texto[2][1] = 0x66;
+	texto[3][1] = 0x77;
+	texto[0][2] = 0x88;
+	texto[1][2] = 0x99;
+	texto[2][2] = 0xAA;
+	texto[3][2] = 0xBB;
+	texto[0][3] = 0xCC;
+	texto[1][3] = 0xDD;
+	texto[2][3] = 0xEE;
+	texto[3][3] = 0xFF;
+
+	cout << "===============" << " CIFRADO RIJNDAEL " << "===============" << endl;
+	cout << "Clave cifrante: ";
+	for (int i=0; i<4; i++){
+		for (int j=0; j<4; j++)
+			cout << hex << setfill('0') << setw(2) << int(clave[j][i]);
+	}
+	cout << endl << "Texto a cifrar: ";
+	for (int i=0; i<4; i++){
+		for (int j=0; j<4; j++)
+			cout << hex << setfill('0') << setw(2) << int(texto[j][i]);
+	}
+	
+	aes rij(clave,texto,0);
+
+	std::vector<vector<unsigned char>> clave_2(4, vector<unsigned char>(4));
+
+	clave_2[0][0] = 0x00;
+	clave_2[1][0] = 0x00;
+	clave_2[2][0] = 0x00;
+	clave_2[3][0] = 0x00;
+	clave_2[0][1] = 0x00;
+	clave_2[1][1] = 0x00;
+	clave_2[2][1] = 0x00;
+	clave_2[3][1] = 0x00;
+	clave_2[0][2] = 0x00;
+	clave_2[1][2] = 0x00;
+	clave_2[2][2] = 0x00;
+	clave_2[3][2] = 0x00;
+	clave_2[0][3] = 0x00;
+	clave_2[1][3] = 0x00;
+	clave_2[2][3] = 0x00;
+	clave_2[3][3] = 0x00;
+
+	std::vector<vector<unsigned char>> texto_2(4, vector<unsigned char>(4));
+
+	texto_2[0][0] = 0x00;
+	texto_2[1][0] = 0x01;
+	texto_2[2][0] = 0x02;
+	texto_2[3][0] = 0x03;
+	texto_2[0][1] = 0x04;
+	texto_2[1][1] = 0x05;
+	texto_2[2][1] = 0x06;
+	texto_2[3][1] = 0x07;
+	texto_2[0][2] = 0x08;
+	texto_2[1][2] = 0x09;
+	texto_2[2][2] = 0x0A;
+	texto_2[3][2] = 0x0B;
+	texto_2[0][3] = 0x0C;
+	texto_2[1][3] = 0x0D;
+	texto_2[2][3] = 0x0E;
+	texto_2[3][3] = 0x0F;
+
+	cout << endl << "===============" << " CIFRADO RIJNDAEL " << "===============" << endl;
+	cout << "Clave cifrante: ";
+	for (int i=0; i<4; i++){
+		for (int j=0; j<4; j++)
+			cout << hex << setfill('0') << setw(2) << int(clave_2[j][i]);
+	}
+	cout << endl << "Texto a cifrar: ";
+	for (int i=0; i<4; i++){
+		for (int j=0; j<4; j++)
+			cout << hex << setfill('0') << setw(2) << int(texto_2[j][i]);
+	}
+
+	aes rij_2(clave_2,texto_2,0);
+
+	std::vector<vector<unsigned char>> clave_3(4, vector<unsigned char>(4));
+
+	clave_3[0][0] = 0x00;
+	clave_3[1][0] = 0x00;
+	clave_3[2][0] = 0x00;
+	clave_3[3][0] = 0x00;
+	clave_3[0][1] = 0x00;
+	clave_3[1][1] = 0x00;
+	clave_3[2][1] = 0x00;
+	clave_3[3][1] = 0x00;
+	clave_3[0][2] = 0x00;
+	clave_3[1][2] = 0x00;
+	clave_3[2][2] = 0x00;
+	clave_3[3][2] = 0x00;
+	clave_3[0][3] = 0x00;
+	clave_3[1][3] = 0x00;
+	clave_3[2][3] = 0x00;
+	clave_3[3][3] = 0x00;
+
+	std::vector<vector<unsigned char>> texto_3(4, vector<unsigned char>(4));
+
+	texto_3[0][0] = 0x00;
+	texto_3[1][0] = 0x01;
+	texto_3[2][0] = 0x02;
+	texto_3[3][0] = 0x03;
+	texto_3[0][1] = 0x04;
+	texto_3[1][1] = 0x05;
+	texto_3[2][1] = 0x06;
+	texto_3[3][1] = 0x07;
+	texto_3[0][2] = 0x08;
+	texto_3[1][2] = 0x09;
+	texto_3[2][2] = 0x0A;
+	texto_3[3][2] = 0x0B;
+	texto_3[0][3] = 0x0C;
+	texto_3[1][3] = 0x0D;
+	texto_3[2][3] = 0x0E;
+	texto_3[3][3] = 0x0F;
+
+	cout << endl << "===============" << " CIFRADO RIJNDAEL " << "===============" << endl;
+	cout << "Clave cifrante: ";
+	for (int i=0; i<4; i++){
+		for (int j=0; j<4; j++)
+			cout << hex << setfill('0') << setw(2) << int(clave_3[j][i]);
+	}
+	cout << endl << "Texto a cifrar: ";
+	for (int i=0; i<4; i++){
+		for (int j=0; j<4; j++)
+			cout << hex << setfill('0') << setw(2) << int(texto_3[j][i]);
+	}
+
+	aes rij_3(clave_3,texto_3,0);
+
+	//AQUI SE ENCUENTRA LA MODIFICACION DE LA PRACTICA DEL CBC
+	cout << endl << "===========" << " MODIFICACION PRACTICA 6 CBC " << "===========" << endl;
+
+	std::vector<vector<unsigned char>> clave_4(4, vector<unsigned char>(4));
+
+	clave_4[0][0] = 0xC7;
+	clave_4[1][0] = 0x38;
+	clave_4[2][0] = 0xE1;
+	clave_4[3][0] = 0x83;
+	clave_4[0][1] = 0x44;
+	clave_4[1][1] = 0x56;
+	clave_4[2][1] = 0xF9;
+	clave_4[3][1] = 0x15;
+	clave_4[0][2] = 0xF3;
+	clave_4[1][2] = 0x7E;
+	clave_4[2][2] = 0x20;
+	clave_4[3][2] = 0x8D;
+	clave_4[0][3] = 0x90;
+	clave_4[1][3] = 0x34;
+	clave_4[2][3] = 0x1B;
+	clave_4[3][3] = 0x2B;
+
+	std::vector<vector<vector<unsigned char>>> textos(2, vector<vector<unsigned char>>(4, vector<unsigned char>(4)));
+
+	textos[0][0][0] = 0xE8;
+	textos[0][1][0] = 0xB1;
+	textos[0][2][0] = 0x1B;
+	textos[0][3][0] = 0xEA;
+	textos[0][0][1] = 0x4A;
+	textos[0][1][1] = 0x65;
+	textos[0][2][1] = 0x9A;
+	textos[0][3][1] = 0x7E;
+	textos[0][0][2] = 0x22;
+	textos[0][1][2] = 0x4F;
+	textos[0][2][2] = 0x92;
+	textos[0][3][2] = 0x4B;
+	textos[0][0][3] = 0xDA;
+	textos[0][1][3] = 0x8D;
+	textos[0][2][3] = 0x72;
+	textos[0][3][3] = 0x39;
+
+	textos[1][0][0] = 0xDA;
+	textos[1][1][0] = 0x47;
+	textos[1][2][0] = 0x51;
+	textos[1][3][0] = 0x74;
+	textos[1][0][1] = 0x9A;
+	textos[1][1][1] = 0x1D;
+	textos[1][2][1] = 0x45;
+	textos[1][3][1] = 0xC9;
+	textos[1][0][2] = 0x2B;
+	textos[1][1][2] = 0x11;
+	textos[1][2][2] = 0x43;
+	textos[1][3][2] = 0x22;
+	textos[1][0][3] = 0xE5;
+	textos[1][1][3] = 0x10;
+	textos[1][2][3] = 0x36;
+	textos[1][3][3] = 0x65;
+
+	std::vector<vector<unsigned char>> clave_anterior(4, vector<unsigned char>(4));
+
+	clave_anterior[0][0] = 0x00;
+	clave_anterior[1][0] = 0x00;
+	clave_anterior[2][0] = 0x00;
+	clave_anterior[3][0] = 0x00;
+	clave_anterior[0][1] = 0x00;
+	clave_anterior[1][1] = 0x00;
+	clave_anterior[2][1] = 0x00;
+	clave_anterior[3][1] = 0x00;
+	clave_anterior[0][2] = 0x00;
+	clave_anterior[1][2] = 0x00;
+	clave_anterior[2][2] = 0x00;
+	clave_anterior[3][2] = 0x00;
+	clave_anterior[0][3] = 0x00;
+	clave_anterior[1][3] = 0x00;
+	clave_anterior[2][3] = 0x00;
+	clave_anterior[3][3] = 0x00;
+
+	cout << "Clave cifrante:    ";
+	for (int i=0; i<4; i++){
+		for (int j=0; j<4; j++)
+			cout << hex << setfill('0') << setw(2) << int(clave_4[j][i]);
+	}
+	cout << endl;
+
+	for (int i=0; i<2; i++){
+	cout << endl << "Texto a cifrar:    ";
+	for (int j=0; j<4; j++){
+		for (int k=0; k<4; k++)
+			cout << hex << setfill('0') << setw(2) << int(textos[i][k][j]);
+	}
+	cout << endl << "Texto anterior:    ";
+	for (int j=0; j<4; j++){
+		for (int k=0; k<4; k++)
+			cout << hex << setfill('0') << setw(2) << int(clave_anterior[k][j]);
+	}
+		aes rij_4(clave_4,textos[i],clave_anterior);
+		clave_anterior = rij_4.get_clave_anterior();
+
+	cout << endl << "Bloque " << i+1 << " cifrado:  ";
+	for (int j=0; j<4; j++){
+		for (int k=0; k<4; k++)
+			cout << hex << setfill('0') << setw(2) << int(clave_anterior[k][j]);
+	}
+	cout << endl;
+	}
+
+	//AQUI SE ENCUENTRA LA MODIFICACION DE LA PRACTICA DEL CIPHER STEALING
+
+	cout << endl << "=================" << " MODIFICACION PRACTICA 6 CP " << "=================" << endl;
+
+	for (int i=0; i<2; i++){
+		aes rij_5(clave_4, textos[i], 1);
+		
+
+		cout << "Texto a cifrar " << i+1 << ":             ";
+		for (int j=0; j<4; j++){
+			for (int k=0; k<4; k++)
+				cout << hex << setfill('0') << setw(2) << int(textos[i][k][j]);
+		}
+
+		textos[i] = rij_5.get_texto_encriptado();
+
+		cout << endl << "Texto cifrado " << i+1 << ":              ";
+		for (int j=0; j<4; j++){
+			for (int k=0; k<4; k++)
+				cout << hex << setfill('0') << setw(2) << int(textos[i][k][j]);
+		}
+
+		if (i<1){
+
+			cout << endl << "Texto a cifrar " << i+2 << " sin alterar: ";
+			for (int j=0; j<4; j++){
+				for (int k=0; k<4; k++)
+					cout << hex << setfill('0') << setw(2) << int(textos[i+1][k][j]);
+			}
+
+			textos[i+1][2][3] = textos[i][2][3];
+			textos[i+1][3][3] = textos[i][3][3];
+		}
+
+		cout << endl;
+	}
+	swap(textos[1],textos[0]);
+
+	cout << endl << "Comprobación de swap";
+	for (int i=0; i<2; i++){
+		cout << endl << "Texto " << i+1	 << " listo para cifrar: ";
+	for (int j=0; j<4; j++){
+			for (int k=0; k<4; k++)
+				cout << hex << setfill('0') << setw(2) << int(textos[i][k][j]);
+		}
+	}
+
+
+	cout << endl;
+}
